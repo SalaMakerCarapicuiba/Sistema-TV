05140aade928860f5cd5839422d39995fcb10a47
LeonardoPaschoaBraga16 <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2024-08-30 15:30:05 -0300

    30/08/2024 15:29 // API CLIMA INTEGRADO
    Api de clima integrada com Sucesso

--- index/views.py
+++ index/views.py
@@ -1,6 +1,20 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    api_key = '98e4910ec3fd6f86dfbfd39f589051bd'
+    city = 'Carapicuíba'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br'
+    response = requests.get(url)
+    weather_data = response.json()
+    
+    context = {
+        'city': city,
+        'temperature': weather_data['main']['temp'],
+        'description': weather_data['weather'][0]['description'],
+        'temp_min': round(weather_data['main']['temp_min']),
+        'temp_max': round(weather_data['main']['temp_max']),
+    }
+    return render(request, "index.html", context)
 
