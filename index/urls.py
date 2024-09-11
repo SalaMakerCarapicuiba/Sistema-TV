1f4e0ed389474746ff03030b459b774b46cad9c8
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2024-09-11 19:57:54 -0300

    API Atualizando Automaticamente
    API Agora carregará automaticamente

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.home, name="home")
 ]
--- index/views.py
+++ index/views.py
@@ -2,18 +2,18 @@ import requests
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
 def home(request):
     api_key = '98e4910ec3fd6f86dfbfd39f589051bd'
     city = 'Carapicuíba'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br'
+
     try:
         # Fazendo a requisição para a API
         response = requests.get(url)
         response.raise_for_status()  # Verifica se a resposta foi bem-sucedida
         weather_data = response.json()
-        
-        # Processando os dados
+
+        # Dados do clima processados
         context = {
             'city': city,
             'temperature': weather_data['main']['temp'],
@@ -21,15 +21,23 @@ def home(request):
             'temp_min': round(weather_data['main']['temp_min']),
             'temp_max': round(weather_data['main']['temp_max']),
         }
-        return JsonResponse(context)
-    
-    except requests.exceptions.RequestException as e:
-        return JsonResponse({'error': 'Erro ao conectar à API'}, status=500)
-    except requests.exceptions.HTTPError as errh:
-        context = {'error': f"HTTP Error: {errh}"}
-    except requests.exceptions.RequestException as e:
-        context = {'error': "Erro ao conectar à API"}
-    except KeyError:
-        context = {'error': "Erro ao processar os dados da API"}
 
-    return render(request, "index.html", context)
+        # Verifica se a requisição foi feita via AJAX
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse(context)
+
+        # Caso contrário, renderiza o template com os dados
+        return render(request, "index.html", context)
+
+    except requests.exceptions.RequestException as e:
+        # Trata erros de conexão ou HTTP
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'error': 'Erro ao conectar à API'}, status=500)
+        return render(request, "index.html", {'error': 'Erro ao conectar à API'})
+
+    except KeyError:
+        # Trata erros de chave ao processar a resposta da API
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({'error': 'Erro ao processar os dados da API'}, status=500)
+        return render(request, "index.html", {'error': 'Erro ao processar os dados da API'})
+    
