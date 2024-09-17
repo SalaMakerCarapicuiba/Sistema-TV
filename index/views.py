65c2b4f54625590af82162dca9fb06f36ea8e716
enricofs <enricoferreiradossantos@gmail.com>
2024-09-17 20:57:10 -0300

    20:57 17/09/2024 // Ajuste API Clima de hoje e amanhã

--- index/views.py
+++ index/views.py
@@ -7,12 +7,28 @@ import datetime
 def home(request):
     api_key = '98e4910ec3fd6f86dfbfd39f589051bd'
     city = 'Carapicuíba'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br'
-
+    
+    # Usando apenas o endpoint de previsão
+    url_forecast = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric&lang=pt_br'
+    
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        weather_data = response.json()
+        # Obtendo os dados da previsão para os próximos dias, incluindo o clima atual
+        response_forecast = requests.get(url_forecast)
+        response_forecast.raise_for_status()
+        forecast_data = response_forecast.json()
+
+        # Clima atual (usamos a primeira previsão disponível, que é do horário mais próximo)
+        current_weather = forecast_data['list'][0]
+
+        # Extraindo a data de amanhã
+        tomorrow_date = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        
+        # Procurando a previsão de amanhã para o meio-dia (12:00:00)
+        forecast_tomorrow = None
+        for forecast in forecast_data['list']:
+            if tomorrow_date in forecast['dt_txt'] and '12:00:00' in forecast['dt_txt']:
+                forecast_tomorrow = forecast
+                break
 
         # Obtendo a data e hora atual
         agora = datetime.datetime.now()
@@ -22,10 +38,12 @@ def home(request):
             # Para requisições AJAX, retornamos também a data e hora
             return JsonResponse({
                 'city': city,
-                'temperature': weather_data['main']['temp'],
-                'description': weather_data['weather'][0]['description'],
-                'temp_min': round(weather_data['main']['temp_min']),
-                'temp_max': round(weather_data['main']['temp_max']),
+                'temperature': current_weather['main']['temp'],
+                'description': current_weather['weather'][0]['description'],
+                'temp_min': round(current_weather['main']['temp_min']),
+                'temp_max': round(current_weather['main']['temp_max']),
+                'temp_min_tomorrow': round(forecast_tomorrow['main']['temp_min']) if forecast_tomorrow else None,
+                'temp_max_tomorrow': round(forecast_tomorrow['main']['temp_max']) if forecast_tomorrow else None,
                 'agora': agora,  # Inclui o horário atual formatado
                 'data_em_texto': data_em_texto  # Data formatada para exibição
             })
@@ -36,10 +54,12 @@ def home(request):
 
         context = {
             'city': city,
-            'temperature': weather_data['main']['temp'],
-            'description': weather_data['weather'][0]['description'],
-            'temp_min': round(weather_data['main']['temp_min']),
-            'temp_max': round(weather_data['main']['temp_max']),
+            'temperature': current_weather['main']['temp'],
+            'description': current_weather['weather'][0]['description'],
+            'temp_min': round(current_weather['main']['temp_min']),
+            'temp_max': round(current_weather['main']['temp_max']),
+            'temp_min_tomorrow': round(forecast_tomorrow['main']['temp_min']) if forecast_tomorrow else None,
+            'temp_max_tomorrow': round(forecast_tomorrow['main']['temp_max']) if forecast_tomorrow else None,
             'users': users,
             'notices': notices,
             'agora': agora,
